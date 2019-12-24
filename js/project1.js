0e86900413a23f997d32d72d147df9959493299d
karuna.alleat <karuna.alleat@PNC.LAN>
2019-12-24 08:20:46 +0700

    md

--- js/project1.js
+++ js/project1.js
@@ -1,71 +1,96 @@
-function getUrl(){
+// get return url
+function getUrl() {
     var url = "https://raw.githubusercontent.com/radytrainer/test-api/master/test.json";
     return url;
 }
-$(document).ready(function(){
-      getApi();
-      $('#recipies').on('change',function(){
-          var recipes = $('#recipies').val();
-          eachrecipies(recipes);
-      })
+$(document).ready(function () {
+    getApi();
+    $('#recipies').on('change', function () {
+        var recipes = $('#recipies').val();
+        eachrecipies(recipes);
+    });
+    //click on add
+    $("#add").on('click', function () {
+        var number = parseInt($("#member").val());
+        sum(number);
+    });
+    // click on decresse 
+    $("#minus").on('click', function () {
+        var number = parseInt($("#member").val());
+        min(number);
+    });
 });
-function getApi(){
+//Get API
+function getApi() {
     $.ajax({
-        dataType:'json',
+        dataType: 'json',
         url: getUrl(),
         success: (data) => chooserecipies(data.recipes),
         error: () => console.log("can not get recipies"),
     });
 }
+// Get all data of recipes
 var allData = [];
-function chooserecipies(recipes ){
+function chooserecipies(recipes) {
     allData = recipes;
     var option = "";
-    recipes.forEach(item =>{
-     option += `
-     <option value="${item.id}">${item.name}</option>
+    recipes.forEach(item => {
+        option += `
+     <option value="${item.id}"> 
+     <h3>${item.name}</h3> 
+     </option>
      `;
- });
- $('#recipies').append(option);
+    });
+    $('#recipies').append(option);
 }
- $('#dobplacate').hide();
- $('#Ingredients').hide();
- $('#instruction').hide();
- $('#rotler').hide();
- $('#number').hide();
-function eachrecipies(id){
-    allData.forEach(recipes =>{
-        if(recipes.id == id){
-           //show reciies
-           showrecipies(recipes.name, recipes.iconUrl);
-           //show ingreadian
+
+$('#dobplacate').hide();
+$('#cards').hide();
+$('#intss').hide();
+$('#footer').hide();
+//Get Each data
+function eachrecipies(id) {
+    allData.forEach(recipes => {
+        if (recipes.id == id) {
+            //show reciies
+            showrecipies(recipes.name, recipes.iconUrl);
+            //show ingreadian
             showingreadian(recipes.ingredients);
             // show intruction
             showintruction(recipes.instructions);
+            //getperson
+            contain = recipes;
+            oldGuest = recipes.nbGuests;
 
-
+            $('#dobplacate').show();
+            $('#cards').show();
+            $('#intss').show();
         }
     });
-    $('#dobplacate').show();
-     $('#Ingredients').show();
-     $('#instruction').show();
-     $('#rotler').show();
-     $('#number').show();
 }
-function showrecipies(name, img,){
-var result = "";
-result +=`
+$('#table').table.val();
+//Get recipies
+function showrecipies(name, img, ) {
+    var result = "";
+    result += `
     <img src="${img}" width = "50%" class="float-right img-fluid">
-    <h3>${name}</h3><br>
-  
+    <h4 class="text-light">${name}</h4><br><br><br>
 `;
-$('#recipis-result').html(result);
+    $('#recipis-result').html(result);
+    var headerRecipies = `
+<h3 class="text-light">Number of recipies</h3><br>
+`;
+    $('#rep').html(headerRecipies);
 }
-
-function showingreadian(ing){
+// Get ingreadian
+function showingreadian(ing) {
     var ingreadian = "";
-    ing.forEach(element =>{
-            ingreadian += `
+    var headeringreadian = `
+    <h2 class="text-center text-light">Ingredients</h2>
+    `;
+    $('#in').html(headeringreadian);
+    ing.forEach(element => {
+        ingreadian += `
               <tr>
               <td><img src= "${element.iconUrl}" width="20%"></td>
                 <td>${element.quantity}</td>
@@ -74,23 +99,67 @@ function showingreadian(ing){
                 
               </tr>
             `;
-            $('#table').html(ingreadian); 
+        $('#table').html(ingreadian);
+        $('#footer').show();
     });
 }
-
-function showintruction(int){
+// condition of sum
+function sum(number) {
+    var add = parseInt(number) + 1;
+    if (add <= 15) {
+        $("#member").val(add);
+        getPerson(add);
+    }
+}
+//condition of minus
+function min(number) {
+    var min = parseInt(number) - 1;
+    if (min >= 1) {
+        $("#member").val(min);
+        getPerson($('#member').val());
+    }
+}
+// Get Step instruction
+function showintruction(int) {
     var instruction = "";
+    var headerInstruction = `
+    <h2 class="text-center text-light">Instruction</h2>
+    `;
+    $('#ist').html(headerInstruction);
     var step = int.split("<step>");
-     for(let i= 1; i<step.length; i++){
-        instruction +=`
+    for (let i = 1; i < step.length; i++) {
+        instruction += `
          <p class="text-primary"> step ${i} : </p>
          <p>${step[i]}</p>
         `;
 
-     }
-     $('#intructions').html(instruction);
-     
+    }
+    $('#intructions').html(instruction);
 }
+// Get new Person
+function getPerson(person) {
+    var divide;
+    var newQuanlity;
+    var result = "";
+    contain.ingredients.forEach(element => {
+        var { quantity, iconUrl, name, unit } = element;
+        divide = quantity / oldGuest;
+        newQuanlity = divide * person;
+        result += `
+        <table class="striped">
+            <tr>
+            <td><img src="${iconUrl}" style="width:50px"></td>
+            <td id='quantity'>${newQuanlity}</td>
+            <td>${unit[0]}</td>
+            <td>${name}</td>
+            </tr>
+        </table>
+    `;
+    });
+    $("#table").html(result);
+}
+
+
 
 
 
